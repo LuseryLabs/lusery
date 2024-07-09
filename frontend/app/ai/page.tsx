06ce9694c966104faa9f87a222cd92cde12607fe
Sebas Arriaza <sebasarriaza@icloud.com>
2024-07-10 01:00:59 +0200

    Install v0, create landing and reorganize layouts

---
+++ frontend/app/ai/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useEffect, useState } from 'react'
+import BlogList from '@/components/BlogList'
+import { Post } from "../../typings";
+import MainLayout from '@/components/MainLayout';
+
+export default function AI() {
+    const [posts, setPosts] = useState<Post[]>([]);
+
+    async function fetchPosts() {
+        try {
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_GHOST_API_URL}/posts/?key=${process.env.NEXT_PUBLIC_GHOST_API_KEY}&include=authors,tags`
+            );
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+
+            setPosts(data.posts);
+        } catch (error) {
+            console.error("Error fetching posts:", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    return (
+        <div>
+            <MainLayout>
+                <BlogList posts={posts} />
+            </MainLayout>
+        </div>
+    )
+}
