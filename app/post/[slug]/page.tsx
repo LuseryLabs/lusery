5b3d04b9cd17557c115d24f6b781a90aef4aafc4
slajara <lajara.agullo@gmail.com>
2024-08-28 18:24:14 +0100

    PostRecommended component create

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -7,9 +7,11 @@ import PostHeader from "@/components/post/PostHeader";
 import PostContent from "@/components/post/PostContent";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
+import PostRecommended from "@/components/PostRecommended";
 
 const PostPage: React.FC = () => {
   const [post, setPost] = useState<Post | null>(null);
+  const [nextPost, setNextPost] = useState<Post | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const { slug } = useParams();
@@ -38,6 +40,26 @@ const PostPage: React.FC = () => {
     fetchPost();
   }, [slug]);
 
+  useEffect(() => {
+    if (!post) return;
+
+    const fetchNextPost = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_GHOST_API_URL}/posts/?key=${process.env.NEXT_PUBLIC_GHOST_API_KEY}&limit=1&filter=created_at:>'${post.created_at}'&order=created_at asc`
+        );
+        const data = await res.json();
+        if (data.posts && data.posts.length > 0) {
+          setNextPost(data.posts[0]);
+        }
+      } catch (error) {
+        console.error("Error fetching next post:", error);
+      }
+    };
+
+    fetchNextPost();
+  }, [post]);
+
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -65,6 +87,7 @@ const PostPage: React.FC = () => {
 
       <PostContent htmlContent={post.html} />
 
+      {nextPost && <PostRecommended post={nextPost} />}
       <Footer />
     </article>
   );
--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Navbar } from "@/components/Navbar";
 import Banner from "@/components/Banner";
-import { Footer } from "@/components/footer";
+import { Footer } from "@/components/Footer";
 
 
 export const metadata: Metadata = {
